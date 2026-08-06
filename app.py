baa8d7809d030ee6b566a32dd87999314e576e50
KennyLou92 <114062621+KennyLou92@users.noreply.github.com>
2026-08-06 16:22:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from Crypto.Cipher import ChaCha20
 app = Flask(__name__)
 CORS(app)
 
-DEFAULT_URL = "https://cf.tosconfig.com/floorScripts/com.madhead.tos.zh/1b362dca1033b1084ac9b2a54903a5c0-index.data"
+DEFAULT_URL = "https://cf.tosconfig.com/floorScripts/com.madhead.tos.zh/ceca94aa70f9ce262f3446632592148b-index.data"
 encoded_key = "Hw0QCtCMy2SQ91gDNh813jeKXSGfrRvzN1UOIPKIRKY="
 KEY = base64.b64decode(encoded_key)
 
