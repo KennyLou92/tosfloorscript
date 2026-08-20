84226b3fc9a0f99ba1a122f796ad7e6c5fa251b7
KennyLou92 <114062621+KennyLou92@users.noreply.github.com>
2026-08-20 21:04:26 +0800

    Update DEFAULT_URL to new data source

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from Crypto.Cipher import ChaCha20
 app = Flask(__name__)
 CORS(app)
 
-DEFAULT_URL = "https://cf.tosconfig.com/floorScripts/com.madhead.tos.zh/828d3d1738119089dff7ab93ab09a3e1-index.data"
+DEFAULT_URL = "https://cf.tosconfig.com/floorScripts/com.madhead.tos.zh/dba8d56bdfda9b9f1d64c6056df9ee03-index.data"
 encoded_key = "Hw0QCtCMy2SQ91gDNh813jeKXSGfrRvzN1UOIPKIRKY="
 KEY = base64.b64decode(encoded_key)
 
